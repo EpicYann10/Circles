ea0c68e8a4d345a76fe6102e777a8f4f953c323a
Yann <77365827+EpicYann10@users.noreply.github.com>
2022-05-16 20:24:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+from turtle import *
+
+screen = Screen()
+screen.setup(400, 400)
+screen.bgcolor('yellow')
+
+my_style = ("Arial", 40, "bold")
+
+
+hideturtle()
+
+color('green')
+dot(500)
+
+color('purple')
+dot(450)
+
+color('blue')
+dot(400)
+
+color('red')
+dot(350)
+
+color('lightgreen')
+dot(300)
+
+color('lightblue')
+dot(250)
+
+color('pink')
+dot(200)
+
+color('gold')
+dot(150)
+
+color('silver')
+dot(100)
+
+color('lightyellow')
+dot(50)
+
+
+
+color("purple")
+write("Yann", font=my_style, align ='center')
